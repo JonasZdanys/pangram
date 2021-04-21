83f6fa3433c6ec87940ce7482b50a191f0ef4aa5
Jonas Zdanys <jonas.zdanys@teliacompany.com>
2021-04-21 16:58:12 +0300

    complete

---
+++ Pangram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+       int alphabetSize = 26;
+
+        return input
+            .ToLower()
+            .Distinct()
+            .Count(char.IsLetter) == alphabetSize;
+    }
+}
